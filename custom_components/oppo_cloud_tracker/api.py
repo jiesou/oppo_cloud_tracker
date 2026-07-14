2b16b272b7d3cd5115814c5900ce8f695147b8be
jiesou <84175239+jiesou@users.noreply.github.com>
2026-07-15 01:18:42 +0800

    fix: headless=new + anti-detect flags + working ToS dismiss via focus+click+MouseEvent + 120s retry loop

--- custom_components/oppo_cloud_tracker/api.py
+++ custom_components/oppo_cloud_tracker/api.py
@@ -112,6 +112,9 @@ class OppoCloudApiClient:
         try:
             chrome_options = ChromeOptions()
             chrome_options.add_argument("--headless=new")
+            chrome_options.add_argument("--disable-blink-features=AutomationControlled")
+            chrome_options.add_experimental_option("excludeSwitches", ["enable-automation"])
+            chrome_options.add_experimental_option("useAutomationExtension", False)
             chrome_options.add_argument("--disable-dev-shm-usage")
             chrome_options.add_argument("--disable-gpu")
             chrome_options.add_argument("--window-size=1920,1080")
